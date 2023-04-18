880c8afe277502e27c707a6af77162372ff27fe3
Rajee Chezhiyan <rajeswarichezhiyan01@gmail.com>
2023-04-18 16:09:03 -0400

    Fixed DOM render for favbooks.

--- static/js/addFavBook.js
+++ static/js/addFavBook.js
@@ -1,42 +1,65 @@
 'use strict';
 
+const favbutton = document.querySelector('#fav_book')
 
-const addbutton = document.querySelector('.Add_Fav_Book')
-const removebutton = document.querySelector('.Rem_Fav_Book')
 const bkid = location.pathname.split('/').pop()
 
 const url_add = `/add_book?book_id=` + bkid;
-
 const url_remove = `/remove_book?book_id=` + bkid;
 
-if (addbutton) {
 
-    addbutton.addEventListener('click', () => {
+
+favbutton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if (favbutton.innerText === "Add book to MyList") {
         fetch(url_add)
-            .then((response) => response.text())
-            .then((respText) => {
-                console.log(respText)
-                if (respText == 'Success') {
-                    alert('Added book to your list!');
-                }else {
-                    alert(respText);
-                }
-            });
-    });
-
-}
-
-if (removebutton){
-    removebutton.addEventListener('click', () => {
+        .then((response) => response.text())
+        .then((respText) => {
+            console.log(respText)
+            if (respText == 'Success') {
+                alert('Added book to your list!');
+                favbutton.innerText = "Remove book from MyList";
+            }else {
+                alert(respText);
+            }
+        });
+    }
+    else{
         fetch(url_remove)
             .then((response) => response.text())
             .then((respText) => {
                 console.log(respText)
                 if (respText == 'Success') {
                     alert('Removed the book from your list!');
+                    favbutton.innerText = "Add book to MyList";
                 }else {
                     alert(respText);
                 }
             });
+        }
+
     });
-}
+
+
+
+
+
+// if (favbutton.innerText === "Remove book from MyList"){
+
+//     // const favbutton = document.querySelector('#fav_book button')
+
+//     favbutton.addEventListener('click', (evt) => {
+//         evt.preventDefault();
+//         fetch(url_remove)
+//             .then((response) => response.text())
+//             .then((respText) => {
+//                 console.log(respText)
+//                 if (respText == 'Success') {
+//                     alert('Removed the book from your list!');
+//                     favbutton.innerText = "Add book to MyList";
+//                 }else {
+//                     alert(respText);
+//                 }
+//             });
+//     });
+// }
